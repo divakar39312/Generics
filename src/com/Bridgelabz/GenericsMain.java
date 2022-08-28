f2a776dbec3eb5c2d51db54ed0db35fefe131bcc
Divakar <divakar39312@gmail.com>
2022-08-28 20:39:41 +0530

    [Divakar]:max of three float value

--- src/com/Bridgelabz/GenericsMain.java
+++ src/com/Bridgelabz/GenericsMain.java
@@ -3,18 +3,18 @@ package com.Bridgelabz;
 import java.util.Scanner;
 
 public class GenericsMain extends MaximumGenerics {
-		// Scanner Class for user input
-		static Scanner scanner = new Scanner(System.in);
-		/**
-		 * 
-		 * @param args
-		 */
-		public static void main(String[] args) {
-			System.out.println("Maximum of three number");
-			// Taking 3 numbers from user
-			Integer x = scanner.nextInt();
-			Integer y = scanner.nextInt();
-			Integer z = scanner.nextInt();
-			System.out.println("Maximum is  " + maximum(x, y, z));
-		}
+	// Scanner Class for user input
+	static Scanner scanner = new Scanner(System.in);
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Maximum of three number");
+		// Taking 3 Float values from user
+		Float x = scanner.nextFloat();
+		Float y = scanner.nextFloat();
+		Float z = scanner.nextFloat();
+		System.out.println("Max: " + maxfloat(x, y, z));
+	}
 }
--- src/com/Bridgelabz/MaximumGenerics.java
+++ src/com/Bridgelabz/MaximumGenerics.java
@@ -1,15 +1,14 @@
 package com.Bridgelabz;
 
 public class MaximumGenerics {
-
 		/**
 		 * @param x taking first value
 		 * @param y taking second value
 		 * @param z taking third value
 		 * @return returning the result which is maximum
 		 */
-		public static int maximum(Integer x, Integer y, Integer z) {
-			Integer max = x;
+		public static Float maxfloat(Float x, Float y, Float z) {
+			Float max = x;
 			if (y.compareTo(max) > 0 && y.compareTo(z) > 0)
 				max = y;
 			else if (z.compareTo(max) > 0)
