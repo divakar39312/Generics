8639f7b9e309a6d8bb9be46a03f2dcea39b94f42
Divakar <divakar39312@gmail.com>
2022-08-29 09:37:33 +0530

    [Divakar]:using genericmethod added

--- src/com/Bridgelabz/MaximumGenerics.java
+++ src/com/Bridgelabz/MaximumGenerics.java
@@ -1,31 +1,22 @@
 package com.Bridgelabz;
 
 public class MaximumGenerics<T extends Comparable<T>> {
-
 	/**
+	 * 
 	 * @param <T>
-	 * @param w   - Taking the first value
-	 * @param x   - Taking the second value
-	 * @param y   - Taking the third value
-	 * @param z   - Taking the fourth value
-	 * @return maximum of 4 values
+	 * @param x   - Taking the first value
+	 * @param y   - Taking the second value
+	 * @param z   - Taking the third value
+	 * @return maximum of 3 values
 	 */
-	public static <T extends Comparable<T>> T toPrintMax(T w, T x, T y, T z) {
-		T max = w;
-		
-		if (x.compareTo(max) > 0 && x.compareTo(y) > 0 && x.compareTo(z) > 0) {
-			max = x;
-		}
-		
-		if (y.compareTo(max) > 0 && y.compareTo(x) > 0 && x.compareTo(z) > 0) {
+	public static <T extends Comparable<T>> T toPrintMax(T x, T y, T z) {
+		T max = x;
+		if (y.compareTo(max) > 0 && y.compareTo(z) > 0) {
 			max = y;
 		}
-		
-		if (z.compareTo(max) > 0 && z.compareTo(x) > 0 && z.compareTo(y) > 0) {
+		if (z.compareTo(max) > 0) {
 			max = z;
 		}
 		return max;
 	}
-	 public static <T> void printMax(T w,T x,T y,T z, T max){
-	        System.out.println(max);
-	    }}
+}
