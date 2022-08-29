39718774c9995bbafdedad9d50013b0deecc0827
Divakar <divakar39312@gmail.com>
2022-08-29 13:24:56 +0530

    [Divakar]:file added

--- src/com/Bridgelabz/GenericsMain.java
+++ src/com/Bridgelabz/GenericsMain.java
@@ -12,5 +12,6 @@ public class GenericsMain {
 		System.out.println("Maximum of Integers is "+MaximumGenerics.toPrintMax(10,20,18));
 		System.out.println("Maximum of Float is " +MaximumGenerics.toPrintMax(10.7f,20.4f,17.7f));
 		System.out.println("Maximum of three Strings is " +MaximumGenerics.toPrintMax("Mango","orange","grapes"));
+	
 	}
 }
